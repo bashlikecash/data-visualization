a6ede9f9a7e956df07fda71208a43f3c4c1d6a49
bashlikesebastian <156124430+bashlikesebastian@users.noreply.github.com>
2024-11-05 11:58:36 -0800

    updates

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import * as cdc from 'https://cdn.jsdelivr.net/gh/stdlib-js/datasets-cdc-nchs-us-births-1994-2003@esm/index.mjs';
 
-window.onload = setup
+//const data = datasets('CDC_NCHS_US_BIRTHS_1994_2003')
 
 // wait for the html elements to be selectable
+window.onload = setup
+
 function setup () {
+    console.log(cdc)
+
     const div = d3.select('#main-vis') // matches the tag in the html file
     
     const dataViz = [
@@ -31,4 +36,26 @@ function setup () {
     // append child is a javascript method not d3.
     // this is just one way to get the update
     div.node().appendChild(svg.node())
+
+    const cdcdiv = document.querySelector('#cdc-vis')
+
+    const cdc_data = cdc.default()
+
+    const svg2 = d3.create('svg')
+        .attr('width', 800)
+        .attr('height', 600)
+
+    //const cdcyears = cdc_data.filter( d => d.year == 1994)
+
+    const cdcyears = cdc_data.reduce( (buckets, row) => {
+        var val = row['year'] // check for the key 'year'
+        if(! val in buckets){ 
+          buckets[val] = [] // if it doesnt have 1994, start a new array
+        }
+        buckets[val].push(row) // append this row to the new array
+        return buckets
+        }, {})
+    
+    return cdcyears
+    
 }
